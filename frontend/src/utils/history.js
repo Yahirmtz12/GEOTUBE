c27f24533974db6b124911a932f9950b3c14236f
Alexis Garcia <21161600@itoaxaca.edu.mx>
2025-10-23 18:44:13 -0600

    Funcionamiento Completo

--- frontend/src/utils/history.js
+++ frontend/src/utils/history.js
@@ -1,7 +1,9 @@
 // frontend/src/utils/history.js
 import axios from 'axios';
+import API_BASE from '../utils/api';
 
-const API_BASE_URL = 'http://localhost:5000/api';
+
+const API_BASE_URL = `${API_BASE}/api`;
 
 export const addVideoToUserHistory = async (video) => {
   const token = localStorage.getItem('token');
